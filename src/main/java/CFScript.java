6b01598e9ee5b4312f4402d7a3878b8c806f55cf
Jared Beck <jared@jaredbeck.com>
2013-10-03 00:03:25 -0400

    extracts class CFScript
    responsible for knowing how to print cfscript expressions.  tightly coupled to CFMLParser.

---
+++ src/main/java/CFScript.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.apache.commons.lang3.StringUtils;
+
+public class CFScript {
+
+  public static String expressionToString(CFMLParser.ExpressionContext ctx) {
+    String str = null;
+    if (ctx.binaryOp() != null) {
+      str = binaryOpToString(ctx.binaryOp());
+    }
+    if (ctx.operand() != null) {
+      str = operandToString(ctx.operand());
+    }
+    return str;
+  }
+
+  // Private Methods
+  // ---------------
+
+  private static String binaryOpToString(CFMLParser.BinaryOpContext ctx) {
+    String lhs = operandToString(ctx.operand());
+    String rhs = expressionToString(ctx.expression());
+    String op = ctx.BINARY_OPERATOR().getText();
+    return String.format("%s %s %s", lhs, op, rhs);
+  }
+
+  private static String operandToString(CFMLParser.OperandContext ctx) {
+    String str = null;
+    if (ctx.literal() != null) {
+      str = ctx.literal().getText();
+    }
+    else if (ctx.reference() != null) {
+      str = referenceToString(ctx.reference());
+    }
+    else if (ctx.funcInvoc() != null) {
+      str = funcInvocToString(ctx.funcInvoc());
+    }
+    return str;
+  }
+
+  private static String referenceToString(CFMLParser.ReferenceContext ctx) {
+    String str = null;
+    if (ctx.dottedRef() != null) {
+      str = ctx.dottedRef().getText();
+    }
+    else if (ctx.arrayIndex() != null) {
+      str = arrayIndexToString(ctx.arrayIndex());
+    }
+    return str;
+  }
+
+  private static String arrayIndexToString(CFMLParser.ArrayIndexContext ctx) {
+    String ref = ctx.dottedRef().getText();
+    String ix = expressionToString(ctx.expression());
+    return String.format("%s[%s]", ref, ix);
+  }
+
+  private static String funcInvocToString(CFMLParser.FuncInvocContext ctx) {
+    String ref = ctx.dottedRef().getText();
+    String args = "";
+    if (ctx.positionalArguments() != null) {
+      args = positionalArgumentsToString(ctx.positionalArguments());
+    }
+    return String.format("%s(%s)", ref, args);
+  }
+
+  private static String positionalArgumentsToString(CFMLParser.PositionalArgumentsContext ctx) {
+    Iterator<CFMLParser.ExpressionContext> ei = ctx.expression().iterator();
+    ArrayList strs = new ArrayList();
+    while(ei.hasNext()) { strs.add(expressionToString(ei.next())); }
+    return StringUtils.join(strs.toArray(), ", ");
+  }
+}
--- src/main/java/CFScriptifyListener.java
+++ src/main/java/CFScriptifyListener.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -228,14 +226,14 @@ public class CFScriptifyListener extends CFMLBaseListener {
 
 	@Override public void enterTagSet(CFMLParser.TagSetContext ctx) {
 		if (ctx.expression() != null) {
-			print(expressionToString(ctx.expression()));
+			print(CFScript.expressionToString(ctx.expression()));
 		}
 		/* else it's an assignment, see enterAssignment */
 	}
 
 	@Override public void enterAssignment(CFMLParser.AssignmentContext ctx) {
 		String lhs = ctx.reference().getText();
-		String rhs = expressionToString(ctx.expression());
+		String rhs = CFScript.expressionToString(ctx.expression());
 		print(String.format("%s = %s", lhs, rhs));
 	}
 
@@ -249,71 +247,6 @@ public class CFScriptifyListener extends CFMLBaseListener {
 
 	@Override public void exitTagAbort(CFMLParser.TagAbortContext ctx) {
 		print("abort");
-	}
-
-	private String expressionToString(CFMLParser.ExpressionContext ctx) {
-		String str = null;
-		if (ctx.binaryOp() != null) {
-			str = binaryOpToString(ctx.binaryOp());
-		}
-		if (ctx.operand() != null) {
-			str = operandToString(ctx.operand());
-		}
-		return str;
-	}
-
-	private String binaryOpToString(CFMLParser.BinaryOpContext ctx) {
-		String lhs = operandToString(ctx.operand());
-		String rhs = expressionToString(ctx.expression());
-		String op = ctx.BINARY_OPERATOR().getText();
-		return String.format("%s %s %s", lhs, op, rhs);
-	}
-
-	private String operandToString(CFMLParser.OperandContext ctx) {
-		String str = null;
-		if (ctx.literal() != null) {
-			str = ctx.literal().getText();
-		}
-		else if (ctx.reference() != null) {
-			str = referenceToString(ctx.reference());
-		}
-		else if (ctx.funcInvoc() != null) {
-			str = funcInvocToString(ctx.funcInvoc());
-		}
-		return str;
-	}
-
-	private String referenceToString(CFMLParser.ReferenceContext ctx) {
-		String str = null;
-		if (ctx.dottedRef() != null) {
-			str = ctx.dottedRef().getText();
-		}
-		else if (ctx.arrayIndex() != null) {
-			str = arrayIndexToString(ctx.arrayIndex());
-		}
-		return str;
-	}
-
-	private String arrayIndexToString(CFMLParser.ArrayIndexContext ctx) {
-		String ref = ctx.dottedRef().getText();
-		String ix = expressionToString(ctx.expression());
-		return String.format("%s[%s]", ref, ix);
-	}
-
-	private String funcInvocToString(CFMLParser.FuncInvocContext ctx) {
-		String ref = ctx.dottedRef().getText();
-		String args = "";
-		if (ctx.positionalArguments() != null) {
-			args = positionalArgumentsToString(ctx.positionalArguments());
-		}
-		return String.format("%s(%s)", ref, args);
-	}
-
-	private String positionalArgumentsToString(CFMLParser.PositionalArgumentsContext ctx) {
-		Iterator<CFMLParser.ExpressionContext> ei = ctx.expression().iterator();
-		ArrayList strs = new ArrayList();
-		while(ei.hasNext()) { strs.add(expressionToString(ei.next())); }
-		return StringUtils.join(strs.toArray(), ", ");
 	}
 
 	private String atrVal(String assignment) {
