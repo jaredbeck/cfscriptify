82b099fdfdaabfae9ec1802e8d1d50954b3070af
Jared Beck <jared@jaredbeck.com>
2013-11-03 18:44:57 -0500

    simplifies run.sh by discarding CLI arguments

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# `abs_path_to_cfs_jar` - returns bsolute path to the .jar
+# `abs_path_to_cfs_jar` - returns absolute path to the .jar
 # built by `mvn assembly:assembly`
 function abs_path_to_cfs_jar {
   local jar_filename="cfscriptify-0.0.1-jar-with-dependencies.jar"
@@ -17,20 +17,15 @@ function dir_of_this_script {
 function cfscriptify {
   local jar="$(abs_path_to_cfs_jar)"
   verify_jar_exists "$jar"
-
-  # Pass stdin to java with `<&0`, and arguments with "$@".
-  # `main()` takes zero arguments, but pass them anyway
-  # so `checkUsage()` can print a helpful message.
-  java -jar "$jar" "$@" <&0
+  java -jar "$jar" <&0
 }
 
 function verify_jar_exists {
-  local jar="$1"
-  if [ ! -f "$jar" ]; then
-    echo "File not found: $jar" 1>&2
+  if [ ! -f "$1" ]; then
+    echo "File not found: $1" 1>&2
     echo 'Did you run `mvn assembly:assembly` yet?' 1>&2
     exit 1
   fi
 }
 
-cfscriptify "$@"
+cfscriptify
